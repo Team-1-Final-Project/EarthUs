294385b934c304f1fec77e286b5ac06dc3f1e04b
Minseok Kim <kms1061@naver.com>
2022-09-20 18:31:07 +0900

    [feat] kakao code발급

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from 'pages/MainPage';
 import BoardList from 'components/board/BoardList';
 import BoardDetail from 'components/board/BoardDetail';
+import KakaoAuth from 'components/login/KakaoAuth';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/board" element={<BoardList />} />
         <Route path="/board/:id" element={<BoardDetail />} />
+        <Route path="/kauth" element={<KakaoAuth />} />
       </Routes>
     </BrowserRouter>
   );
