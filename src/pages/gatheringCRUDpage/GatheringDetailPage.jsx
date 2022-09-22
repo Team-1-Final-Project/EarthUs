2fb8f67babbe5a8b4e3f4881a24a313680199b1c
Minseok Kim <kms1061@naver.com>
2022-09-22 11:12:09 +0900

    [Docs] 모임 디테일 페이지관련  서브라우터 수정

---
+++ src/pages/gatheringCRUDpage/GatheringDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardCreateForm from 'components/gathering/create/CardCreateForm';
+
+const GatheringDetailPage = () => {
+  return (
+    <div>
+      <CardCreateForm />
+    </div>
+  );
+};
+
+export default GatheringDetailPage;
--- src/shared/sub/GatheringRoute.jsx
+++ src/shared/sub/GatheringRoute.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import GatheringPage from 'pages/GatheringPage';
 import GatheringCreatePage from 'pages/gatheringCRUDpage/GatheringCreatePage';
+import GatheringDetailPage from 'pages/gatheringCRUDpage/GatheringDetailPage';
 
 const GatheringRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<GatheringPage />} />
       <Route path="/create" element={<GatheringCreatePage />} />
+      <Route path="/detail" element={<GatheringDetailPage />} />
     </Routes>
   );
 };
