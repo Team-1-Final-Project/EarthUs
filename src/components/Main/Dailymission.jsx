cee55e143926d9eec4952333a5368c973f98561b
JUNGJIN <ojj824@naver.com>
2022-09-23 02:20:52 +0900

    [Design] mainpage UI 수정

--- src/components/Main/Dailymission.jsx
+++ src/components/Main/Dailymission.jsx
@@ -4,7 +4,7 @@ function Dailymission({ mission }) {
   return (
     <div className="flex justify-center">
       {mission && (
-        <div className="flex flex-col items-center justify-center w-9/12 h-40 mt-8 border-2 rounded-3xl">
+        <div className="flex flex-col items-center justify-center w-9/12 h-40 mt-8 border-2 rounded-xl">
           <div className="text-3xl">{mission[0].dailyMission}</div>
           <div>성공한 인원은 {mission[0].successUser}명 입니다.</div>
           <button className="p-2 border-2 rounded-3xl hover:bg-green-600 hover:text-white">
