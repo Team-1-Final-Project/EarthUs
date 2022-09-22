ef2a477b2eb2c7e2a7851215f0814cd240bb87b7
JUNGJIN <ojj824@naver.com>
2022-09-23 02:33:31 +0900

    [Design] Navbar 수정

--- src/components/Navbar/ Navbar.jsx
+++ src/components/Navbar/ Navbar.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+  const list = ['zerowaste', 'community', 'zeromoim', 'zeroshop'];
+
   return (
     <nav className="">
       <div className="max-w-6xl px-4 mx-auto">
+        <div className="flex items-center justify-center h-20">
+          <div className="text-3xl font-bold text-defaultColor">Earth,us</div>
+        </div>
         <div className="flex items-center justify-between h-16">
-          {/* 메뉴 */}
-          <div className="flex flex-row items-center gap-8">
-            <div className="text-2xl font-bold text-green-600">Logo</div>
-            <div className="">커뮤니티</div>
-            <div>모임</div>
-            <div>추천샵</div>
-          </div>
-          {/* 메뉴2 */}
-          <div>로그인</div>
+          {list.map((list) => (
+            <div
+              className="text-defaultText hover:cursor-pointer hover:text-defaultColor"
+              onClick={() => {
+                navigate(`${list}`);
+              }}
+            >
+              {list}
+            </div>
+          ))}
         </div>
       </div>
     </nav>
