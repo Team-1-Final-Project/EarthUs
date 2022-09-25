edeae4b885460cb3c384e78e5712914ef2340ff0
Minseok Kim <kms1061@naver.com>
2022-09-25 23:14:23 +0900

    [feat] 모임 생성 폼 변경

--- src/components/meeting/create/CardCreateForm.jsx
+++ src/components/meeting/create/CardCreateForm.jsx
@@ -1,6 +1,8 @@
 import { useInput } from 'hooks/useInput';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { apis } from 'api/api';
+import Preview from './Preview';
 
 export default function CardCreateForm() {
   const navigate = useNavigate();
@@ -13,7 +15,7 @@ export default function CardCreateForm() {
   const [meetingStartDate, meetingStartDateChange] = useInput('');
   const [meetingEndDate, meetingEndDateChange] = useInput('');
   const [content, contentChange] = useInput('');
-  const [image, imageChange] = useInput('');
+  const [image, setImage] = useState('');
 
   const list = [2, 3, 4, 5, 6, 7, 8];
 
@@ -56,6 +58,10 @@ export default function CardCreateForm() {
     }
   };
 
+  const onChangeImageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+
   return (
     <>
       <div className="mt-20 flex justify-center">
@@ -67,20 +73,25 @@ export default function CardCreateForm() {
                 <label className="mt-10 block text-sm font-medium text-gray-700">사진 등록</label>
                 <div className="mt-1 h-full flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
                   <div className="space-y-1 text-center">
-                    <svg
-                      className="mx-auto h-12 w-12 text-gray-400"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
+                    {image ? (
+                      <Preview img={image} />
+                    ) : (
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    )}
+
                     <div className="flex text-sm text-gray-600">
                       <label
                         htmlFor="file-upload"
@@ -92,13 +103,13 @@ export default function CardCreateForm() {
                           name="file-upload"
                           type="file"
                           className="sr-only"
-                          value={image}
-                          onChange={imageChange}
+                          onChange={(e) => {
+                            onChangeImageHandler(e);
+                          }}
                         />
                       </label>
                       <p className="pl-1">or drag and drop</p>
                     </div>
-                    <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                   </div>
                 </div>
               </div>
