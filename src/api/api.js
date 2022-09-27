cba51070ea6d0c87263ce9b440052fa3638c8ed7
JUNGJIN <ojj824@naver.com>
2022-09-28 03:18:17 +0900

    remove all cache

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `http://localhost:3001`,
+  baseURL: `http://3.37.61.61/`,
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     accept: 'application/json,',
