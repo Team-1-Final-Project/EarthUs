35ce13c5ee088da2f17cd46d586cd2c5b1a16a35
JUNGJIN <ojj824@naver.com>
2022-09-28 03:28:11 +0900

    [Feat] remove all cache

--- src/redux/configStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import loginSlice from './modules/loginSlice';
-
-const store = configureStore({
-  reducer: {
-    login: loginSlice.reducer,
-  },
-});
-export default store;
