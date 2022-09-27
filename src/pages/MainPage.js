78fdb857527c91f779f2cd5d07566db0d6676027
JUNGJIN <ojj824@naver.com>
2022-09-27 22:31:17 +0900

    [Feat] 깃헙 대소문자 구분

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,7 +4,6 @@ import Dailymission from 'components/main/Dailymission';
 import { apis } from 'api/api';
 import TopPost from 'components/main/TopPost';
 import Gather from 'components/main/Gather';
-// import LoginGoogle from 'components/login/LoginGoogle';
 import Banner from 'components/banner/Banner';
 import Footer from 'components/footer/Footer';
 
