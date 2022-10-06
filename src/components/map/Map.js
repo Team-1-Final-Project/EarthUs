88dba67836e2734482e1877ffe086024fef2b39b
Minseok Kim <kms1061@naver.com>
2022-10-06 16:30:35 +0900

    [feat] 카카오 맵

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,15 +1,58 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { MapMarker, Map } from 'react-kakao-maps-sdk';
+import { useState, useEffect } from 'react';
+import { Container, Layout } from 'utils/styles/GlobalStyles';
 
 // import { useState } from 'react';
 
 const KakaoMap = () => {
+  const { kakao } = window;
+  const [info, setInfo] = useState();
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState();
+
+  useEffect(() => {
+    const places = new kakao.maps.services.Places();
+    places.keywordSearch('대림창고', (data, status, _pagination) => {
+      //데이터는 검색결과임
+      if (status === kakao.maps.services.Status.OK) {
+        //검색결과가 있으면
+        const bounds = new kakao.maps.LatLngBounds(); //bounds는 빈 좌표정보 변수임
+        let markers = [];
+
+        markers.push({
+          //마커스에 최상단에 있는 검색결과의 좌표를 담는다
+          position: {
+            lat: data[0].y,
+            lng: data[0].x,
+          },
+          content: data[0].place_name, //content에 상호명을 담는다
+        });
+        bounds.extend(new kakao.maps.LatLng(data[0].y, data[0].x)); //바운즈에 최상단 좌표정보를 담아
+
+        setMarkers(markers); //마커스를 바꿔준다
+        map.setBounds(bounds); //지도의 좌표를 변환시켜준다.
+      }
+    });
+  }, [map]);
+
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '500px', height: '500px' }}>
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: '#000' }}>Hello World!</div>
-      </MapMarker>
-    </Map>
+    <Layout>
+      <Container>
+        {markers.map((item) => (
+          <Map
+            center={{ lat: 37.54478312746583, lng: 127.05670308843499 }}
+            style={{ width: '400px', height: '400px' }}
+            level={3}
+            onCreate={setMap}
+          >
+            <MapMarker position={item.position}>
+              <div className="bg-white">대림창고</div>
+            </MapMarker>
+          </Map>
+        ))}
+      </Container>
+    </Layout>
   );
 };
 
