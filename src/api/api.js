38ef8db74db4bb9e5708267b283d57a69a5adfd6
JUNGJIN <ojj824@naver.com>
2022-10-14 21:24:31 +0900

    [Feat] server 주소 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `http://3.37.61.61/`,
+  baseURL: `${process.env.REACT_APP_SERVER}`,
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     accept: 'application/json,',
