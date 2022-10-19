ff81a817c72da7cdf062bf28e65e0a3a397e1a8b
JUNGJIN <ojj824@naver.com>
2022-10-20 03:44:48 +0900

    [Fix] Bug 수정 및 tooltip 기능 완료

--- src/components/mypage/Badge.jsx
+++ src/components/mypage/Badge.jsx
@@ -9,23 +9,25 @@ function Badge({ myBadge }) {
     <div>
       <div className="m-4 mt-10 text-xl font-bold">획득한 뱃지</div>
       <div className="flex justify-center">
-        <div className="grid w-8/12 grid-cols-2 gap-10 sm:grid-cols-3 md:grid-cols-4 justify-items-center">
+        <div className="grid w-8/12 grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 justify-items-center">
           {badges.map((badge, index) => (
             <>
-              <div key={badge.id}>
+              <div
+                key={badge.id}
+                data-tip={badge.content}
+                data-for="myBadge"
+                className="flex flex-col items-center justify-center"
+              >
                 <img
                   className={`w-20 h-20 bg-gray-100 rounded-full ${
                     successId?.includes(index) ? null : 'grayscale'
                   }`}
                   src={badge.image}
                   alt="badgeImage"
-                  data-tip
-                  data-for="myBadge"
                 />
+                <span className="px-2 mt-2 text-sm shadow-xl rounded-3xl">{badge.title}</span>
               </div>
-              <ReactTooltip id="myBadge">
-                <span>{badge.content}</span>
-              </ReactTooltip>
+              <ReactTooltip id="myBadge" getContent={(dataTip) => `${dataTip}`} />
             </>
           ))}
         </div>
