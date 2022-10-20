977e1251f920bbefaff8583b0a2eca5adbcf1f8f
JUNGJIN <ojj824@naver.com>
2022-10-21 00:26:19 +0900

    [Fix] zeroshop address 수정

--- src/components/shop/ShopList.jsx
+++ src/components/shop/ShopList.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
 
 function ShopList({ shop }) {
-  console.log(shop);
   return (
     <div className="grid grid-cols-1 gap-2 place-content-center">
       {shop &&
         shop.map((shop) => (
-          <>
-            <div
-              className="flex flex-row w-full h-48 text-center shadow-lg default_outline "
-              key={shop.id}
-            >
-              <div className="w-4/12">
-                <img className="h-48 rounded-lg " src={shop.image} alt="shopImage" />
-              </div>
-              <div className="flex flex-col justify-center w-full gap-4 ml-2">
-                <div className="mt-2 text-2xl font-bold">{shop.shopName}</div>
-                <div className="mr-2 text-sm">{shop.content}</div>
-                <div className="">
-                  <button
-                    onClick={() => window.open(`${shop.link}`, '_blank')}
-                    className="p-1 w-28 default_outline hover_button"
-                  >
-                    사이트 이동
-                  </button>
-                </div>
+          <div
+            className="flex flex-row w-full h-48 text-center shadow-lg default_outline "
+            key={shop.title}
+          >
+            <div className="w-4/12">
+              <img className="h-48 rounded-lg " src={shop.image} alt="shopImage" />
+            </div>
+            <div className="flex flex-col justify-center w-full gap-4 ml-2">
+              <div className="mt-2 text-2xl font-bold">{shop.title}</div>
+              <div className="mr-2 text-sm">{shop.content}</div>
+              <div className="">
+                <button
+                  onClick={() => window.open(`${shop.address}`, '_blank')}
+                  className="p-1 w-28 default_outline hover_button"
+                >
+                  사이트 이동
+                </button>
               </div>
             </div>
-          </>
+          </div>
         ))}
     </div>
   );
