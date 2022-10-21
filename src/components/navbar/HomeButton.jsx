33f0e95a217388051ca0a0f67b4990ac2f670340
Minseok Kim <kms1061@naver.com>
2022-10-21 19:26:49 +0900

    [Design] 로고 디자인 변경

--- src/components/navbar/HomeButton.jsx
+++ src/components/navbar/HomeButton.jsx
@@ -8,12 +8,12 @@ function HomeButton() {
       <div className="max-w-6xl px-4 mx-auto">
         <Link to="/">
           <div className="flex items-center justify-center h-20">
-            <img src={Logo} className="w-10 h-10 rounded-full mr-3" />
+            <img src={Logo} className="w-10 h-10 mr-2" />
             <div
-              className="text-3xl font-bold text-defaultColor"
-              style={{ fontFamily: 'Pretendard-ExtraBold' }}
+              className="text-4xl mt-2 text-[#52A4E2]"
+              style={{ fontFamily: 'HiMelody-Regular' }}
             >
-              Earth,us
+              Earth, us
             </div>
           </div>
         </Link>
