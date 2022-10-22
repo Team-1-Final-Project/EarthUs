7e2e8d16ee67cdeaad62e698ae56faf8640d4583
simyuseon <ysoen0508@naver.com>
2022-10-23 04:43:25 +0900

    [Feat] 모임 댓글 구현완료

--- src/components/meeting/comment/DeleteMeetingComment.jsx
+++ src/components/meeting/comment/DeleteMeetingComment.jsx
@@ -1,4 +1,18 @@
-const DeleteMeetingComment = () => {
-  return <div></div>;
+import { apis } from 'api/api';
+import { BsTrash } from 'react-icons/bs';
+
+const DeleteMeetingComment = ({ data, setState }) => {
+  return (
+    <div>
+      <BsTrash
+        className="button cursor-pointer"
+        onClick={() => {
+          apis.deleteMeetingComment(data.commentId).then((res) => {
+            setState('deletet');
+          });
+        }}
+      />
+    </div>
+  );
 };
 export default DeleteMeetingComment;
