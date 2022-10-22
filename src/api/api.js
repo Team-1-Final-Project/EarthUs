2cede5cd292ed8876c4c0b2ae3e339117672166c
simyuseon <ysoen0508@naver.com>
2022-10-22 18:00:46 +0900

    [Feat] 누락된 코드 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `http://54.180.116.99/`,
+  baseURL: `${process.env.REACT_APP_SERVER}`,
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     accept: 'application/json,',
