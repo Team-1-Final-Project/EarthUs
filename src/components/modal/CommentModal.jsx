89e590e3586e89d9c6528a0ff27d85d208b17e37
simyuseon <ysoen0508@naver.com>
2022-10-24 12:50:38 +0900

    [Feat] 모임댓글 수정 모달창으로 변경

---
+++ src/components/modal/CommentModal.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Backdrop = ({ onConfirm }) => {
+  return (
+    <div className="z-50 w-full h-full fixed top-0 left-0 bg-modalBackground" onClick={onConfirm} />
+  );
+};
+
+const ModalOverlay = ({ children }) => {
+  return (
+    <div
+      className={`z-50 w-3/5 h-4/5  overflow-scroll rounded-lg shadow-2xl bg-white top-1/2 left-1/2 fixed translate-x-[-50%] translate-y-[-50%] flex flex-col justify-center items-center `}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CommentModal = ({ children, onConfirm }) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{children}</ModalOverlay>,
+        document.getElementById('overlay-root')
+      )}
+    </>
+  );
+};
+
+export default CommentModal;
